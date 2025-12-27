5ea9aadd5e1450090514d788fbb8302c784bbcc5
Debyendu Roy <roydebyendu1006@gmail.com>
2025-12-27 20:30:34 +0530

    Add files via upload

---
+++ MicroServices/DiscoveryServer/src/main/java/com/debyendu/discoveryserver/DiscoveryserverApplication.java
@@ -0,0 +1,16 @@
+package com.debyendu.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryserverApplication {
+
+	public static void main(String[] args) {
+		// Tell Boot to look for registration-server.yml
+		System.setProperty("spring.config.name", "registration-server");
+		SpringApplication.run(DiscoveryserverApplication.class, args);
+	}
+}
